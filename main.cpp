1f96a4588e80d4259e829492e6d0575abab83ccd
git <charlie.smith@thebuytoletbusiness.com>
2018-02-13 17:20:32 +0000

    Modified main to include failed grade function.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,20 @@ using std::endl;			using std::string;
 using std::setprecision;	using std::vector;
 using std::domain_error;	using std::max;
 
+vector<Student_info> extract_fails(vector<Student_info>& students){
+	vector<Student_info>::size_type fail;
+	vector<Student_info>::size_type i = 0;
 
+	while (i != students.size()){
+		if (fgrade(students[i])){
+			fail.push_back(students[i]);
+			students.erase(students.begin() + i);
+		} else {
+			++i;
+		}
+	}
+	return fail;
+}
 
 int main() {
 	vector<Student_info> students;
